38639f1d6cd15ef2152d3c0c9da68a18aeb00d5f
Daniel <danielsilva.ita@gmail.com>
2014-02-01 19:07:25 -0500

    training implemented: cycle dumps rare words only

--- src/NBTrain.java
+++ src/NBTrain.java
@@ -6,10 +6,15 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.Vector;
 
 
 public class NBTrain {
+	
+	public static int MAX_WORDS_IN_MEMORY = 200000;
+	public static int MIN_COUNT_TO_REMAIN = 2;
+	
 	
 	static Vector<String> tokenizeDoc(String cur_doc) {
         String[] words = cur_doc.split("\\s+");
@@ -26,23 +31,24 @@ public class NBTrain {
 	
 	public static void main(String[] args) {
 		
-		
-		
 		//vector of dictionaries: one hashmap per class
 		Vector<HashMap<String,Integer>> vectorOfDics= new Vector<HashMap<String,Integer>>(); 
 		//hashmap that links ClassName to array number of vectorOfDics
 		HashMap<String,Integer> classPosition = new HashMap<String,Integer>();
 		
-		//labels of interest
-		String[] sL = new String[]{"CCAT","ECAT","GCAT","MCAT"};
-		HashSet<String> selectedLabels = new HashSet<String>(Arrays.asList(sL));
-		
 		//dic for current label
 		HashMap<String,Integer> currentDic;
+		int totalSeenClasses = 0;
 		
 		//total documents
-		int totalCount = 0;
-		ArrayList<Integer> totalLabelDocs = new ArrayList<Integer>(); 
+		int totalDocCount = 0;
+		ArrayList<Integer> totalLabelDocs = new ArrayList<Integer>();
+		
+		//total words
+		ArrayList<Integer> totalLabelWords = new ArrayList<Integer>();
+		
+		//variables for limiting memory usage
+		int wordsInMemory = 0;
 		
 		// +--------------------------------------------------------
 		// |     READING AND COUNTING (TRAINING)
@@ -52,12 +58,12 @@ public class NBTrain {
 		// | we will only count them once data is consolidated
 		// +--------------------------------------------------------
 		
-		//long startTime = System.currentTimeMillis(); 
+		long startTime = System.currentTimeMillis(); 
 		try {
 			
-			//String inputPath = args[0];
-	        //BufferedReader br = new BufferedReader(new FileReader(inputPath));
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String inputPath = args[0];
+	        BufferedReader br = new BufferedReader(new FileReader(inputPath));
+	        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	        String line = br.readLine();
 	        
 			while (line != null) {
@@ -67,23 +73,17 @@ public class NBTrain {
 				String[] labels = labelsAndTokens[0].split(",");
 				Vector<String> tokens = tokenizeDoc(labelsAndTokens[1]);
 				
-				//filter out unselected labels
-				Vector<String> filteredLabels = new Vector<String>();
-				for(String label : labels){
-					if(selectedLabels.contains(label))
-						filteredLabels.add(label);
-				}
-				
 				//for each label
-				for(String label : filteredLabels){
-					totalCount ++;
+				for(String label : labels){
 					
-					//if is label was previously unseen
+					//if label was previously unseen
 					if(!classPosition.containsKey(label)){
-						int assignedPos = vectorOfDics.size();
+						int assignedPos = totalSeenClasses;
+						totalSeenClasses++;
 						classPosition.put(label, assignedPos);
 						vectorOfDics.add(new HashMap<String,Integer>());
 						totalLabelDocs.add(assignedPos,0);
+						totalLabelWords.add(assignedPos,0);
 					}
 					
 					//count of docs per label
@@ -97,13 +97,59 @@ public class NBTrain {
 					for(String token : tokens){
 						if(!currentDic.containsKey(token)){
 							currentDic.put(token, 1);
+							wordsInMemory++;
 						}
 						else{
 							currentDic.put(token, currentDic.get(token)+1);
 						}
 					}
 					
-				}//end for label			
+				}//end for label
+				
+				
+				//Dump to disk if memory usage is too large
+				while(wordsInMemory > MAX_WORDS_IN_MEMORY){
+					
+					//System.out.println("words in memory:"+wordsInMemory);
+					
+					//for each dictionary
+					for(Entry<String, Integer> dic_e : classPosition.entrySet()){
+						
+						String label = dic_e.getKey();
+						int pos = dic_e.getValue();
+						currentDic = vectorOfDics.elementAt(pos);
+						HashMap<String,Integer> remainingDic = new HashMap<String,Integer>();  
+						
+						//for each label
+						for(Entry<String,Integer> e : currentDic.entrySet()){
+							
+							String token = e.getKey();
+							int tokenCount = e.getValue();
+							if(tokenCount < MIN_COUNT_TO_REMAIN){
+								
+								//output to disk
+								System.out.println("Y="+label+",W="+token+"\t"+tokenCount);
+								//update word counter
+								totalLabelDocs.set(pos, totalLabelDocs.get(pos) + tokenCount);
+
+								wordsInMemory--;
+							}
+							else{
+								remainingDic.put(token, tokenCount);
+							}
+							
+						}
+						vectorOfDics.remove(pos);
+						vectorOfDics.add(pos, remainingDic);
+					}	
+					
+					//System.out.println("words in memory:"+wordsInMemory+"\n----------------");
+					
+					if(wordsInMemory > MAX_WORDS_IN_MEMORY){
+						MIN_COUNT_TO_REMAIN+=1;
+					}
+					
+				}//end of dumping
 				
 				line = br.readLine();	
 			}
@@ -113,50 +159,53 @@ public class NBTrain {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		//long estimatedTime = System.currentTimeMillis() - startTime;
-		//System.out.println("Training Time: "+estimatedTime);
 		
 		
-		// +--------------------------------------------------------
-		// |     OUTPUT COUNTING RESULTS (FOR FUTURE TESTING)
-		// |--------------------------------------------------------
-		// |  We will now count Y=*  and  Y=y|W=* while outputting 
-		// |  the results to stdout
-		// +--------------------------------------------------------
+		//Dump remaining words
+		for(Entry<String, Integer> dic_e : classPosition.entrySet()){
 			
-		
-		//for each label
-		//for(String label : classPosition.keySet()){
-		for(Entry<String, Integer> e : classPosition.entrySet()){
-			String label = e.getKey();
-			int pos = e.getValue();
-			int labelCount = 0;
+			String label = dic_e.getKey();
+			int pos = dic_e.getValue();
 			currentDic = vectorOfDics.elementAt(classPosition.get(label));
 			
-			//output each count of type Y=y,W=w
-			for (Entry<String, Integer> entry : currentDic.entrySet()) {
-			    String token = entry.getKey();
-			    int tokenCount = entry.getValue();
-			    System.out.println("Y="+label+",W="+token+"\t"+tokenCount);
-			    labelCount += tokenCount;
+			//for each label
+			Set<Entry<String,Integer>> allEntries = currentDic.entrySet(); 
+			for(Entry<String,Integer> e : allEntries){
+			    int tokenCount = e.getValue();
+				String token = e.getKey();
+				//output to disk
+				System.out.println("Y="+label+",W="+token+"\t"+tokenCount);
+				//update word counter
+				totalLabelWords.set(pos, totalLabelWords.get(pos) + tokenCount);
 			}
 			
-			//output count of Y=y,W=*
-			System.out.println("Y="+label+",W=*\t"+labelCount);
+			vectorOfDics.elementAt(pos).clear();
+		}	
+		
+		
+		//print label counters
+		for(Entry<String,Integer> e : classPosition.entrySet()){
+			String label = e.getKey();
+			int labelPos = e.getValue();
 			
-			//output count of Y=y
-			System.out.println("Y="+label+"\t"+totalLabelDocs.get(pos));
-			
+			//Y=y, W=*
+			int totalWordsInLabel = totalLabelWords.get(labelPos);
+			System.out.println("Y="+label+",W=*\t"+totalWordsInLabel);
+			//Y=y
+			int totalDocsInLabel = totalLabelDocs.get(labelPos);
+			System.out.println("Y="+label+"\t"+totalDocsInLabel);
+			totalDocCount += totalDocsInLabel;
 		}
-		//output count of Y=*
-		System.out.println("Y=*\t"+totalCount);
-		
-			
-			
-		
-		
 
+		//total doc counter Y=*
+		System.out.println("Y=*\t"+totalDocCount);
+		
+		
+		System.out.println("MIN_COUNT_TO_REMAIN: "+MIN_COUNT_TO_REMAIN);
+		
+		long estimatedTime = System.currentTimeMillis() - startTime;
+		System.out.println("Training Time: "+estimatedTime);
+		
 	}
 
 }
